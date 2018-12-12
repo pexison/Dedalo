5aa71f5c2e3dfe553ad339352972eabcbff92837
John Delgado <pexison@gmail.com>
2018-12-12 12:01:14 +0100

    fix: oh-my-zsh and tmux
    oh-my-zsh is now in the repo and the tmux config file names are now
    starting with dot.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 DOTFILESREPO=~/Dedalo
-VIMDIR=~/.vim
 
 ############################################################################
 # Symlink zsh config
 ############################################################################
-ln -sf $DOTFILESREPO/zsh/.oh-my-zsh ~/.oh-my-zsh
+ln -sf $DOTFILESREPO/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -sf $DOTFILESREPO/zsh/zshrc ~/.zshrc
 
 ############################################################################
 # Symlink tmux config
 ############################################################################
-ln -sf $DOTFILESREPO/tmux/tmux.conf ~/tmux.conf
-ln -sf $DOTFILESREPO/tmux/tmux.airline.conf ~/tmux.airline.conf
+ln -sf $DOTFILESREPO/tmux/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILESREPO/tmux/tmux.airline.conf ~/.tmux.airline.conf
 
 ############################################################################
 # Symlink vim config
 ############################################################################
-ln -sf $DOTFILESREPO/vim $VIMDIR
+ln -sf $DOTFILESREPO/vim ~/.vim
