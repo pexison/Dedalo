746b927e3408a9777b32ec3dfccad63509fc5816
John Delgado <pexison@gmail.com>
2020-06-28 22:22:16 +0200

    deploy: Add new script for deploying the configuration

--- README.md
+++ README.md
@@ -9,5 +9,10 @@ My Daily use configuration for:
 - vim-plug (https://github.com/junegunn/vim-plug)
 ### ZSH
 - oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+
 ## Deploy
-- Run the script: link.sh
+Run the script `deploy.sh` and follow the instructions. This will install all the basic
+dependencies for tha daily work, change the default shell to zsh, link the dotfiles to home
+and install the plugins for VIM.
+
+I know that I always forget to `git clone --recursive`, so there is a fallback for that case ;)
---
+++ deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+read -p "Insert location of the repository if it differs from default [~/Dedalo]: " -s dotfilesrepo
+
+if [ "$dotfilesrepo" = $'\n' ]; then
+  dotfilesrepo=~/Dedalo
+fi;
+
+printf "\nInsert password (`whoami`): "
+read -s uPassword
+
+############################################################################
+# Install most used dependencies
+############################################################################
+printf "%s\n" "$uPassword" |
+sudo --stdin apt install zsh \
+                 gcc \
+                 clang \
+                 perl \
+                 make \
+                 cmake \
+                 vim \
+                 tmux \
+                 trash-cli \
+                 fonts-powerline \
+                 fonts-emojione \
+                 fonts-noto-color-emoji
+
+############################################################################
+# Change system settings
+############################################################################
+sudo -S chsh -s $(which zsh) "$USER" <<< "$uPassword"
+
+############################################################################
+# Recursive clone backup
+############################################################################
+# Always forgetting to clone recursive
+if [ -d $dotfilesrepo/zsh/oh-my-zsh/.git ]; then
+  cd $dotfilesrepo/zsh/oh-my-zsh
+  git submodule update --init --recursive
+  cd $dotfilesrepo/zsh/custom-themes/powerlevel9k
+  git submodule update --init --recursive
+  cd -
+fi;
+
+############################################################################
+# Symlink config
+############################################################################
+# Symlinks for zsh
+ln -sf $dotfilesrepo/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -sf $dotfilesrepo/zsh/zshrc ~/.zshrc
+ln -sf $dotfilesrepo/zsh/zshtheme ~/.zshtheme
+ln -sf $dotfilesrepo/zsh/custom-themes ~/.zsh-custom-themes
+
+# Symlinks for tmux
+ln -sf $dotfilesrepo/tmux/tmux.conf ~/.tmux.conf
+ln -sf $dotfilesrepo/tmux/tmux.airline.conf ~/.tmux.airline.conf
+
+# Symlinks for vim
+ln -sf $dotfilesrepo/vim ~/.vim
+
+############################################################################
+# Install vim plugins
+############################################################################
+vim -c ':PlugUpgrade | PlugInstall | exit | exit'
