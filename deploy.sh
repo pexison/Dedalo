1a32649b6e2db8e823c63094f91994c06032c182
John Delgado <pexison@gmail.com>
2021-02-25 20:38:05 +0100

    general: Add suport for Darwin systems

--- deploy.sh
+++ deploy.sh
@@ -1,66 +1,55 @@
 #!/bin/bash
 
 read -p "Insert location of the repository if it differs from default [~/Dedalo]: " -s dotfilesrepo
-
 if [ -z "$dotfilesrepo" ]; then
   dotfilesrepo=~/Dedalo
 fi;
 
 printf "\nInsert password (`whoami`): "
 read -s uPassword
+echo ""
 
-############################################################################
-# Install most used dependencies
-############################################################################
-printf "%s\n" "$uPassword" |
-sudo --stdin apt install zsh \
-                 gcc \
-                 clang \
-                 perl \
-                 make \
-                 cmake \
-                 vim \
-                 tmux \
-                 trash-cli \
-                 silversearcher-ag \
-                 fonts-powerline \
-                 fonts-emojione \
-                 fonts-noto-color-emoji
-
-############################################################################
-# Change system settings
-############################################################################
-sudo -S chsh -s $(which zsh) "$USER" <<< "$uPassword"
-
-############################################################################
-# Recursive clone backup
-############################################################################
-# Always forgetting to clone recursive
+# Recursive clone backup - always forgetting to clone recursive
 if [ -d $dotfilesrepo/zsh/oh-my-zsh/.git ]; then
-  cd $dotfilesrepo/zsh/oh-my-zsh
-  git submodule update --init --recursive
-  cd $dotfilesrepo/zsh/custom-themes/powerlevel9k
-  git submodule update --init --recursive
-  cd -
-fi;
+  cd $dotfilesrepo && git submodule update --init --recursive && cd -
+fi
 
-############################################################################
-# Symlink config
-############################################################################
-# Symlinks for zsh
+# Determine if the script is running under a Darwin distribution (mac in my case)
+echo "Installing dependencies"
+if [[ ! $(uname -a | grep 'Darwin') ]]; then
+  printf "%s\n" "$uPassword" | sudo --stdin apt install zsh \
+                   gcc \
+                   clang \
+                   perl \
+                   make \
+                   cmake \
+                   vim \
+                   tmux \
+                   trash-cli \
+                   silversearcher-ag \
+                   fonts-powerline \
+                   fonts-emojione \
+                   fonts-noto-color-emoji
+else
+  echo "Installing Homebrew, please extend the deployment script with the next steps"
+  #bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install ag rsnapshot htop tmux thefuck trash-cli
+fi
+
+echo "Changing default shell"
+sudo -S chsh -s $(which zsh) "$USER" <<< "$uPassword"
+echo ""
+
+echo "Creating symlinks"
 ln -sf $dotfilesrepo/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -sf $dotfilesrepo/zsh/zshrc ~/.zshrc
 ln -sf $dotfilesrepo/zsh/zshtheme ~/.zshtheme
 ln -sf $dotfilesrepo/zsh/custom-themes ~/.zsh-custom-themes
-
-# Symlinks for tmux
 ln -sf $dotfilesrepo/tmux/tmux.conf ~/.tmux.conf
 ln -sf $dotfilesrepo/tmux/tmux.airline.conf ~/.tmux.airline.conf
-
-# Symlinks for vim
 ln -sf $dotfilesrepo/vim ~/.vim
 
-############################################################################
-# Install vim plugins
-############################################################################
+echo "Installing vim plugings"
 vim -c ':PlugUpgrade | PlugInstall | exit | exit'
+
+echo "Done!"
